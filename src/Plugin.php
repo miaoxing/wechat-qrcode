23f15d1aa7b3fda17398b66a1c536a42b2c4e7cd
twinh <twinhuang@qq.com>
2018-01-04 10:52:43 +0800

    v1基类移到v2的目录和命名空间 [skip ci]

--- src/Plugin.php
+++ src/Plugin.php
@@ -7,7 +7,7 @@ use Miaoxing\Wechat\Service\WechatAccount;
 use Miaoxing\Wechat\Service\WeChatQrcode;
 use Wei\WeChatApp;
 
-class Plugin extends \miaoxing\plugin\BasePlugin
+class Plugin extends \Miaoxing\Plugin\BasePlugin
 {
     protected $name = '微信二维码';
 
