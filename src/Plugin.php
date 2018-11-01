ed7d79aa4b39465a867313f6fcff5c58fc181275
twinh <twinhuang@qq.com>
2018-11-01 21:33:55 +0800

    feat: 优化逻辑

--- src/Plugin.php
+++ src/Plugin.php
@@ -38,7 +38,7 @@ class Plugin extends \Miaoxing\Plugin\BasePlugin
         }
 
         // 检查二维码是否存在
-        $qrcode = wei()->weChatQrcode()->findOrInit(['sceneId' => $sceneId]);
+        $qrcode = wei()->weChatQrcode->findAndCacheBySceneId($sceneId);
         if ($qrcode->isNew()) {
             return;
         }
